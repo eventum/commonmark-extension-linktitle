47760e95bff8ebf7c1612674606f3c4385fe1606
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 15:25:25 +0200

    Update GitlabUnfurl to match url host

--- src/CommonMark/Extension/LinkTitle/GitlabUnfurl.php
+++ src/CommonMark/Extension/LinkTitle/GitlabUnfurl.php
@@ -6,6 +6,14 @@ use League\CommonMark\Inline\Element\Link;
 
 class GitlabUnfurl implements UnfurlInterface
 {
+    /** @var string */
+    private $domain;
+
+    public function __construct(string $domain)
+    {
+        $this->domain = $domain;
+    }
+
     public function unfurl(Link $link): array
     {
         return [];
@@ -13,6 +21,8 @@ class GitlabUnfurl implements UnfurlInterface
 
     public function accept(Link $link): bool
     {
-        return false;
+        $domain = parse_url($link->getUrl(), PHP_URL_HOST);
+
+        return $domain === $this->domain;
     }
 }
