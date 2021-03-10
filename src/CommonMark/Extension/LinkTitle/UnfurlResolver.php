30e61139ee44f9ff750e4ac20b7c8dc3a39d6e75
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 15:22:06 +0200

    Add UnfurlResolver class

---
+++ src/CommonMark/Extension/LinkTitle/UnfurlResolver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Eventum\Delfi\CommonMark\Extension\LinkTitle;
+
+use League\CommonMark\Inline\Element\Link;
+
+class UnfurlResolver
+{
+    /** @var UnfurlInterface[] */
+    private $resolvers;
+
+    public function __construct(array $resolvers)
+    {
+        $this->resolvers = $resolvers;
+    }
+
+    public function resolve(Link $link): void
+    {
+        foreach ($this->resolvers as $resolver) {
+            if ($resolver->accept($link)) {
+                $resolver->unfurl($link);
+            }
+        }
+    }
+}
