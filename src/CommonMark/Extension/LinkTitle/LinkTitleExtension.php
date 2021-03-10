f0eac7a020bed3098962f230d6a5b061399b7f74
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 14:54:02 +0200

    Update code to intercept Link elements

--- src/CommonMark/Extension/LinkTitle/LinkTitleExtension.php
+++ src/CommonMark/Extension/LinkTitle/LinkTitleExtension.php
@@ -5,11 +5,30 @@ declare(strict_types=1);
 namespace Eventum\Delfi\CommonMark\Extension\LinkTitle;
 
 use League\CommonMark\ConfigurableEnvironmentInterface;
+use League\CommonMark\Event\DocumentParsedEvent;
 use League\CommonMark\Extension\ExtensionInterface;
+use League\CommonMark\Inline\Element\Link;
 
 final class LinkTitleExtension implements ExtensionInterface
 {
     public function register(ConfigurableEnvironmentInterface $environment): void
+    {
+        // Intercept Link elements and add title attribute
+        $environment->addEventListener(DocumentParsedEvent::class, function (DocumentParsedEvent $e): void {
+            $walker = $e->getDocument()->walker();
+
+            while ($event = $walker->next()) {
+                $node = $event->getNode();
+                if (!($node instanceof Link) || !$event->isEntering()) {
+                    continue;
+                }
+
+                $this->linkWalker($node);
+            }
+        });
+    }
+
+    private function linkWalker(Link $link): void
     {
     }
 }
