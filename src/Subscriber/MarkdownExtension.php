3523e45466f249ddc49cb6b3dba2884335c6a7ea
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 15:36:04 +0200

    Add extensions to Subscriber/MarkdownExtension

--- src/Subscriber/MarkdownExtension.php
+++ src/Subscriber/MarkdownExtension.php
@@ -2,30 +2,36 @@
 
 namespace Eventum\Delfi\Subscriber;
 
-use Eventum\Delfi\CommonMark\Extension\LinkTitle\LinkTitleExtension;
 use Eventum\Event\SystemEvents;
 use League\CommonMark\Environment;
+use League\CommonMark\Extension\ExtensionInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 class MarkdownExtension implements EventSubscriberInterface
 {
+    /** @var ExtensionInterface[] */
+    private $extensions;
+
+    public function __construct(array $extensions)
+    {
+        $this->extensions = $extensions;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
-            /** @see markdownExtension */
-            SystemEvents::MARKDOWN_ENVIRONMENT_CONFIGURE => 'markdownExtension',
+            /** @see registerExtensions */
+            SystemEvents::MARKDOWN_ENVIRONMENT_CONFIGURE => 'registerExtensions',
         ];
     }
 
-    public function markdownExtension(GenericEvent $event): void
+    public function registerExtensions(GenericEvent $event): void
     {
+        /** @var Environment $environment */
         $environment = $event->getSubject();
-        $this->applyExtensions($environment);
-    }
-
-    private function applyExtensions(Environment $environment): void
-    {
-        $environment->addExtension(new LinkTitleExtension());
+        foreach ($this->extensions as $extension) {
+            $environment->addExtension($extension);
+        }
     }
 }
