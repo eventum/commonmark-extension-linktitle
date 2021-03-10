8743168bd4db8e6fa86e7419cba4a5885a528398
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 16:06:31 +0200

    Update GitlabUnfurl to update issue title for link

--- src/CommonMark/Extension/LinkTitle/GitlabUnfurl.php
+++ src/CommonMark/Extension/LinkTitle/GitlabUnfurl.php
@@ -2,21 +2,33 @@
 
 namespace Eventum\Delfi\CommonMark\Extension\LinkTitle;
 
+use Eventum\Delfi\GitlabClient;
 use League\CommonMark\Inline\Element\Link;
+use UnexpectedValueException;
 
 class GitlabUnfurl implements UnfurlInterface
 {
     /** @var string */
     private $domain;
+    /** @var GitlabClient */
+    private $client;
 
-    public function __construct(string $domain)
-    {
+    public function __construct(
+        GitlabClient $client,
+        string $domain
+    ) {
         $this->domain = $domain;
+        $this->client = $client;
     }
 
     public function unfurl(Link $link): array
     {
-        return [];
+        [$projectPath, $issueId] = $this->getProjectPathAndIssueId($link->getUrl());
+        $result = $this->client->getIssue($projectPath, $issueId);
+
+        return [
+            'title' => $result['title'],
+        ];
     }
 
     public function accept(Link $link): bool
@@ -24,5 +36,23 @@ class GitlabUnfurl implements UnfurlInterface
         $domain = parse_url($link->getUrl(), PHP_URL_HOST);
 
         return $domain === $this->domain;
+    }
+
+    /**
+     * Get Project path and Issue Id from link
+     */
+    private function getProjectPathAndIssueId(string $url): array
+    {
+        $path = parse_url($url, PHP_URL_PATH);
+        $parts = explode('/-/issues/', $path, 2);
+        if (count($parts) !== 2) {
+            $parts = explode('/issues/', $path, 2);
+        }
+        if (count($parts) !== 2) {
+            throw new UnexpectedValueException("Unable to parse: {$path}");
+        }
+        [$projectPath, $issueId] = $parts;
+
+        return [ltrim($projectPath, '/'), $issueId];
     }
 }
