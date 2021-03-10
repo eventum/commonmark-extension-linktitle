9eeffa8b346b0d36150f6b9413f729fbcc86be5f
Elan Ruusamäe <glen@delfi.ee>
2021-03-10 14:46:23 +0200

    Add MarkdownExtension subscriber

---
+++ src/Subscriber/MarkdownExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Eventum\Delfi\Subscriber;
+
+use Eventum\Event\SystemEvents;
+use League\CommonMark\Environment;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class MarkdownExtension implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            /** @see markdownExtension */
+            SystemEvents::MARKDOWN_ENVIRONMENT_CONFIGURE => 'markdownExtension',
+        ];
+    }
+
+    public function markdownExtension(GenericEvent $event): void
+    {
+        $environment = $event->getSubject();
+        $this->applyExtensions($environment);
+    }
+
+    private function applyExtensions(Environment $environment): void
+    {
+    }
+}
